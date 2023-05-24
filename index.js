cd610bb9245d8546c4fae6ce1ca08ccc454be2cd
Michael Tobechukwu Nwokike <76704861+Michael-Tobechukwu@users.noreply.github.com>
2023-05-24 19:22:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,29 +12,32 @@
 
     // Populate the areas select based on the selected option
     if (selectedOption === "Amazing Grace") {
-      populateOptions(["Adonai", "Mighty Favour", "Amazing Grace"]);
-    } else if (selectedOption === "Bethel") {
-      populateOptions(["Bethel Area 1", "Bethel Area 2" ]);
+      populateOptions(["","Adonai", "Mighty Favour", "Amazing Grace"]);
+    } else if (selectedOption === ""){
+      populateOptions(["Select your zone to see the list of areas",])
+    }
+    else if (selectedOption === "Bethel") {
+      populateOptions(["","Bethel Area 1", "Bethel Area 2" ]);
     } else if (selectedOption === "Celebration") {
-      populateOptions(["Celebration Area 1", "Celebration Option 2", "Celebration Area 3"]);
+      populateOptions(["","Celebration Area 1", "Celebration Option 2", "Celebration Area 3"]);
     } else if (selectedOption === "Excellence") {
-      populateOptions(["Excellence Area 1", "Excellence Area 2", "Excellence Area 3"]);
+      populateOptions(["","Excellence Area 1", "Excellence Area 2", "Excellence Area 3"]);
     } else if (selectedOption === "Light House") {
-      populateOptions(["Light House Area 1", "Light House Area 2"]);
+      populateOptions(["","Light House Area 1", "Light House Area 2"]);
     } else if (selectedOption === "Livingstone") {
-      populateOptions(["Livingstone Area 1", "Livingstone Area 2", "Livingstone Area 3"]);
+      populateOptions(["","Livingstone Area 1", "Livingstone Area 2", "Livingstone Area 3"]);
     } else if (selectedOption === "Garden of Love") {
-      populateOptions(["Garden of Love Area 1", "Garden of Love Area 2"]);
+      populateOptions(["","Garden of Love Area 1", "Garden of Love Area 2"]);
     } else if (selectedOption === "Peace Sanctuary") {
-      populateOptions(["Peace Sanctuary Parish"]);
+      populateOptions(["","Peace Sanctuary Parish"]);
     } else if (selectedOption === "Solid Rock") {
-      populateOptions(["Solid Rock Area 1", "Solid Rock Area 2", "Solid Rock Area 3"]);
+      populateOptions(["","Solid Rock Area 1", "Solid Rock Area 2", "Solid Rock Area 3"]);
     } else if (selectedOption === "Sunshine") {
-      populateOptions(["Sunshine Area 1", "Sunshine Area 2"]);
+      populateOptions(["","Sunshine Area 1", "Sunshine Area 2"]);
     } else if (selectedOption === "Victory Arena") {
-      populateOptions(["Victory Arena Area 1", "Victory Arena Area 2", "Victory Arena Area 3"]);
+      populateOptions(["","Victory Arena Area 1", "Victory Arena Area 2", "Victory Arena Area 3"]);
     } else if (selectedOption === "Wonderland") {
-      populateOptions(["Wonderland Area 1", "Wonderland Option 2", "Wonderland Area 3", "Wonderland Area 4"]);
+      populateOptions(["","Wonderland Area 1", "Wonderland Option 2", "Wonderland Area 3", "Wonderland Area 4"]);
     }
     
   });
@@ -95,12 +98,35 @@
   }
 
 
-//Prompt to alert on submit
-var submitBtn = document.getElementById('submitBtn');
+//Show form for second parish
+const parishData2 = document.getElementById('newParish2');
+const newParishBtn = document.getElementById('newParishBtn');
 
-function submitAlert(){
-    alert('Are you sure you want to submit? You cannot make changes after submitting.')
+function showParishData2() {
+  parishData2.style.display = 'block';
+  parishData2.innerHTML = `
+  <hr>
+<h5>Parish 2</h5>
+<input type="text" name="parish" id="parish" placeholder="Enter parish name" required>
+<div class="attendance">
+  <p>Attendance</p>
+  <input type="number" placeholder="Men" id="men" required>
+  <input type="number" placeholder="Women" id="women" required>
+  <input type="number" placeholder="Children" id="children" required>
+</div>
+<p id="totalAttendance"></p>
+<input type="number" name="offering" id="offering" required placeholder="Enter total offering">
+<input type="button" value="Click to see amount to remit" onclick="calculateAnswer()" class="calculateBtn">
+<p id="result"></p>
+
+<div class="comment">
+  <textarea name="commentService" id="commentService" cols="" rows="4" placeholder="Comments on the service"></textarea>
+</div>
+<input type="button" value="Click to add report for another parish" id="newParishBtn">
+
+
+`
 }
 
-submitBtn.addEventListener('click', submitAlert)
+newParishBtn.addEventListener('click', showParishData2)
 
